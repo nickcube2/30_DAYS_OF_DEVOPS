6efebc17315caf92491be7876eb60b2d88bf5307
nickcube2 <awuninicholas@gmail.com>
2026-01-29 11:27:00 -0700

    Initial commit

---
+++ platform/infra/terraform/envs/dev/main.tf
@@ -0,0 +1,61 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  cluster_endpoint_public_access_cidrs = [
+    "172.59.227.54/32"
+  ]
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 2
+      min_size       = 2
+      max_size       = 3
+    }
+  }
+
+  tags = {
+    Environment = var.env
+  }
+}
---
+++ platform/infra/terraform/envs/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "nick-eks-dev"
+}
